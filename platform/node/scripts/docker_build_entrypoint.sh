ab270a63692db9614a38657d39a3388178374705
acalcutt <acalcutt@techidiots.net>
2022-10-22 13:58:07 -0400

    try to fix build

--- platform/node/scripts/docker_build_entrypoint.sh
+++ platform/node/scripts/docker_build_entrypoint.sh
@@ -5,8 +5,8 @@ cat /etc/os-release
 uname -m
 
 cd /usr/src/app
-npm ci --ignore-scripts
 npm run build
+npm install
 cp -R ./lib/ /data/lib/
 
 xvfb-run --auto-servernum ./build/mbgl-render-test-runner --manifestPath metrics/macos-xcode11-release-style.json
