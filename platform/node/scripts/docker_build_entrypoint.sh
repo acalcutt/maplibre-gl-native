90ee9a864a29cbdf1a141f4f7484ce455bda0ac9
acalcutt <acalcutt@techidiots.net>
2022-12-28 12:51:27 -0500

    try to avoid copying files

--- platform/node/scripts/docker_build_entrypoint.sh
+++ platform/node/scripts/docker_build_entrypoint.sh
@@ -4,9 +4,8 @@ node -v
 cat /etc/os-release
 uname -m
 
-cd /usr/src/app
+cd /data
 npm run build
-cp -R ./lib/ /data/lib/
 
 xvfb-run --auto-servernum ./build/mbgl-render-test-runner --manifestPath metrics/macos-xcode11-release-style.json
 xvfb-run --auto-servernum npm test
