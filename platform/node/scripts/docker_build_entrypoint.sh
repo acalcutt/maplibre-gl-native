ba8f3932350cfef6ea84f87ccce9401f36317604
acalcutt <acalcutt@techidiots.net>
2022-12-28 22:25:48 -0500

    Revert "try to avoid copying files"
    This reverts commit 90ee9a864a29cbdf1a141f4f7484ce455bda0ac9.

--- platform/node/scripts/docker_build_entrypoint.sh
+++ platform/node/scripts/docker_build_entrypoint.sh
@@ -4,8 +4,9 @@ node -v
 cat /etc/os-release
 uname -m
 
-cd /data
+cd /usr/src/app
 npm run build
+cp -R ./lib/ /data/lib/
 
 xvfb-run --auto-servernum ./build/mbgl-render-test-runner --manifestPath metrics/macos-xcode11-release-style.json
 xvfb-run --auto-servernum npm test
