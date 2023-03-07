65c6fbdf75fb9215ac4fd2e1c4d5441faf6ce227
acalcutt <acalcutt@techidiots.net>
2023-03-07 11:45:07 -0500

    Revert "try to fix SetZoom SetCenter setBearing setDebug"
    This reverts commit d2cd8daa6d8512a4e733cabe0ea4d3bf0d1a8ffc.

--- platform/node/src/node_map.cpp
+++ platform/node/src/node_map.cpp
@@ -465,26 +465,11 @@ void NodeMap::startRender(const NodeMap::RenderOptions& options) {
     frontend->setSize(options.size);
     map->setSize(options.size);
 
-    if (map->getZoom() != options.zoom) {
-        map->setZoom(options.zoom);
-    }
-
-    mbgl::LatLng latLng(options.latitude, options.longitude);
-    if (map->getLatLng() != latLng) {
-        map->setLatLng(latLng);
-    }
-
-    if (map->getBearing() != options.bearing) {
-        map->setBearing(options.bearing);
-    }
-
-    if (map->getPitch() != options.pitch) {
-        map->setPitch(options.pitch);
-    }
-
-    if (map->getDebug() != options.debugOptions) {
-        map->setDebug(options.debugOptions);
-    }
+    mbgl::CameraOptions camera;
+    camera.center = mbgl::LatLng { options.latitude, options.longitude };
+    camera.zoom = options.zoom;
+    camera.bearing = options.bearing;
+    camera.pitch = options.pitch;
 
     auto projectionOptions = mbgl::ProjectionMode()
         .withAxonometric(options.axonometric)
